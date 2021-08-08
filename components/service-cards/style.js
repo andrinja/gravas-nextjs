b5dc11a43c26c4bc463ba1f1a901f9f3d4aed631
Andra Zeberga <anze@moodagent.com>
2021-08-08 21:34:30 +0200

    remove margin

--- components/service-cards/style.js
+++ components/service-cards/style.js
@@ -4,7 +4,6 @@ const Cards = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr ));
     grid-gap: 3rem;
-    margin: 24px auto;
 `
 
 export default Cards;
