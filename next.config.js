265984ced8342f5dcd987938697093b9e0ea0b13
andrinja <andra.zeberga@gmail.com>
2026-02-28 14:56:30 +0100

    remove next-offline

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 const withOptimizedImages = require("next-optimized-images");
 const { i18n } = require('./next-i18next.config');
 
-const withOffline = require("next-offline");
+// Next.js only needs these i18n keys; localePath is for next-i18next
+const { defaultLocale, locales, localeDetection } = i18n;
+
+// next-offline removed: incompatible with Next.js 13/Webpack 5 (mkdirp API changed)
+// For PWA/offline support, consider migrating to Serwist
 
 const nextConfig = {
-	i18n,
-	workboxOpts: {
-    swDest: '../public/service-worker.js',
-  },
+	i18n: { defaultLocale, locales, localeDetection },
 };
 
-module.exports = withOffline(withOptimizedImages(nextConfig));
+module.exports = withOptimizedImages(nextConfig);
